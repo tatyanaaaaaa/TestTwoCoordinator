3f0974bfe0864a40700396166c931f646189440a
Sosina Tatyana <fedorova_t95@mail.ru>
2022-04-20 12:24:39 +0300

    Initial Commit

---
+++ TestTwoCoordinator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTwoCoordinator
+//
+//  Created by Tatiana Sosina on 20.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
